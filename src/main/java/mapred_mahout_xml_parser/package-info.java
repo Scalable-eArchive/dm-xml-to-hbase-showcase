69c116690fb7ffafcc613d41a6f85012efac5005
Peter Kofler <peter.kofler@code-cop.org>
2014-08-01 16:42:34 +0200

    add description to Mahout XML parser example

---
+++ src/main/java/mapred_mahout_xml_parser/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Use the Mahout example XMLParser.
+ * The parser is in the mahout-examples up to version 0.2.
+ * The latest Mahout is 0.9 which still depends on Hadoop 1.x
+ * @see http://xmlandhadoop.blogspot.com.au/2010/08/xml-processing-in-hadoop.html
+ */
+package mapred_mahout_xml_parser;
+
